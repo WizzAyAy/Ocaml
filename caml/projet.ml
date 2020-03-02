b869fe7063e6562aed8f59f998209c795a9c9ce1
WizzAyAy <55085753+WizzAyAy@users.noreply.github.com>
2020-03-02 14:06:04 +0100

    Add files via upload

--- caml/projet.ml
+++ caml/projet.ml
@@ -1,7 +1,9 @@
 (**PARTIE UNE**)
 let graphe1 = [(1,[6;7;8]); (2,[1;4]); (3, [2]); (4, [3;5]); (5, [1]); (6, [5;7]); (7, []); (8, [6;7])];;
+let graphesanscycle = [(1, [2;3]); (2, [3]); (3, [])];;
 
 (**1**)
+(*nous donne la liste des sommet d'un graphe*)
 let rec liste_sommet_graphe graphe =
 	match graphe with
 	[] -> []
@@ -10,6 +12,7 @@ let rec liste_sommet_graphe graphe =
 (*liste_sommet_graphe graphe1;;*)
 
 (**2**)
+(*nous donne la liste des succ d'un sommet donné*)
 let rec liste_succ graphe elem =
 	match graphe with 
 	[] -> failwith ("ce sommet n'existe pas")
@@ -18,6 +21,7 @@ let rec liste_succ graphe elem =
 (*liste_succ graphe1 1;;*)
 
 (**3**)
+(*ajoute un arc a un graphe*)
 let rec ajoutelem graphe elem = 
 	match graphe,elem with
 	[],_ -> elem::graphe
@@ -25,44 +29,62 @@ let rec ajoutelem graphe elem =
 		if x = ns 
 		then (x,l@li)::reste
 		else (x,l) :: ajoutelem reste elem;;
-	
+		
+(*combine deux graphe ne garde pas les redondances*)
 let rec combiner g1 g2 =
 	List.fold_left (ajoutelem) g1 g2;;
-	
+
+(*inverse un graphe*)
 let rec inverser graphe =
 	List.fold_left (fun base (s,lisuc) -> 
 		combiner base  (List.fold_left (fun base2 ei -> ajoutelem base2 (ei,[s]) ) [] lisuc)
 	) [] graphe;;
+	
+(*genre de bubble sort juste pour avoir les sommets dans le bon ordre'*)
+let rec tri graphe =
+	match graphe with
+	[] -> []
+	| (a,x)::(b,y)::reste -> if a < b 
+	then (a,x) :: (tri ((b,y)::reste)) 
+	else (tri ((b,y)::(a,x)::reste))
+	| (a,x)::[] -> (a,x)::[];;
+	
+let bbsort graphe =
+	let rec tmp graphe n =
+	if n > 0
+	then tmp (tri graphe) (n-1)
+	else graphe
+in tmp graphe (List.length graphe / 2 + 1);;
 
+let graphe_inv = bbsort (inverser graphe1);;
 
-(*inverser graphe1;;*)
 	
 (**4**)
-let rec appartient li e =
-	match li with
-	[] -> false
-	| x :: r -> if e = x then true else appartient r e;;
-
-let sommet noeud =
-	match noeud with
-	(x,li) -> x;;
-
-let premierNoeud graphe =
-	match graphe with
-	[] -> failwith "pas de premier Noeud"
-	| x::_ -> x;;
-
+(*nous donne le noeud a partir de son id*)
 let rec getNode graphe sommet =
 	match graphe with
 	[] -> failwith "pas de sommet"
 	| (s,li)::reste -> if s = sommet then (s,li) else getNode reste sommet;;
 
-let parcours graphe = 
+(*parcours suffixe d'un graphe*)
+let parcours graphe =
+(*liste de sommets deja visite et sommet courant*)
 	let rec visite listeDejaVisit (s,li) =
-		if appartient listeDejaVisit s
-			then listeDejaVisit
-			else List.fold_left (fun base ei -> visite (s::base) (getNode graphe ei)) listeDejaVisit li 
-	in visite [] (getNode graphe 2);;
+	(*si la liste des succ est vide alors je place le sommet*)
+	match li with
+	[] -> if List.mem s listeDejaVisit
+		then listeDejaVisit
+		else s::listeDejaVisit
+	(*si elle ne l'est pas alors je visite ses succs*)
+	| _ ->
+		List.fold_left (fun base ei ->
+		if List.mem s base
+ 			then visite base (getNode graphe ei)
+			else visite (s::base) (getNode graphe ei) 
+		) listeDejaVisit li
+		
+in visite [] (getNode graphe 1);;
+
 parcours graphe1;;
 
 
